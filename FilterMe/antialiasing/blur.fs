47a031ba8b9c47c14a2f0db2aa413df18ee07f29
samoatesgames <samoatesgames@75805131-b4cb-4ddf-9f19-764025d2d572>
2013-02-25 15:11:42 +0000

    Basic filtering code

---
+++ FilterMe/antialiasing/blur.fs
@@ -0,0 +1,44 @@
+#include "antialiasing/edge-detetion.fs"
+
+vec3 simpleBlur(sampler2D sampler, ivec2 pixel_coord, int kernal)
+{
+	vec3 pixel_colour;
+	int noofSamples = 0;
+	
+	for (int x = -kernal; x <= kernal; ++x)
+	{
+		for (int y = -kernal; y <= kernal; ++y)
+		{
+			ivec2 offset = ivec2(x, y);
+			pixel_colour += texelFetch(sampler_pixel, pixel_coord + offset, 0).rgb;
+			noofSamples++;
+		}
+	}
+	
+	return pixel_colour / noofSamples;
+}
+
+vec3 edgeBlur( sampler2D sampler, ivec2 pixel_coord, int kernal)
+{
+	vec3 edge_color = edgeDetection(sampler_pixel, pixel_coord);
+
+	if (edge_color.x == 0)
+	{
+		return texelFetch(sampler_pixel, pixel_coord, 0).rgb;
+	}
+
+	vec3 pixel_colour;
+	int noofSamples = 0;
+	
+	for (int x = -kernal; x <= kernal; ++x)
+	{
+		for (int y = -kernal; y <= kernal; ++y)
+		{
+			ivec2 offset = ivec2(x, y);
+			pixel_colour += texelFetch(sampler_pixel, pixel_coord + offset, 0).rgb;
+			noofSamples++;
+		}
+	}
+	
+	return pixel_colour / noofSamples;
+}
