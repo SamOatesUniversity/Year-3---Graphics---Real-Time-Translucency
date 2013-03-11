4155e152f2721a9447b39b0a4d8270002a6deff2
samoatesgames <samoatesgames@75805131-b4cb-4ddf-9f19-764025d2d572>
2013-03-11 15:48:37 +0000

    Added SMAA shader.

--- trunk/shaders/postprocessing_fs.glsl
+++ trunk/shaders/postprocessing_fs.glsl
@@ -2,14 +2,18 @@
 
 #include "antialiasing/blur.fs"
 
+#define SMAA_PIXEL_SIZE float2(1.0 / 1280.0, 1.0 / 720.0)
+#define SMAA_GLSL_3 1
+#define SMAA_PRESET_LOW 1
+
+#include "antialiasing/SMAA.fs"
+
 uniform sampler2D sampler_pixel;
 out vec4 fragment_colour;
 
 void main(void)
 {
     ivec2 pixel_coord = ivec2(gl_FragCoord.xy);
-
-	vec3 pixel_colour = edgeBlur(sampler_pixel, pixel_coord, 1);
-	
+    vec3 pixel_colour = edgeBlur(sampler_pixel, pixel_coord, 1);
     fragment_colour = vec4(pixel_colour, 0.0f);
 }
