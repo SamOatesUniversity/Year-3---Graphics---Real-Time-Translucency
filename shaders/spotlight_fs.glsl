a63c89b055d07657c1be84616e94a01137215e77
samoatesgames <samoatesgames@75805131-b4cb-4ddf-9f19-764025d2d572>
2013-03-20 14:47:45 +0000

    Removed majority of shadow code. Lets get the shadow map depth rendering first.

--- shaders/spotlight_fs.glsl
+++ shaders/spotlight_fs.glsl
@@ -7,17 +7,12 @@ uniform sampler2DRect sampler_world_tangent;
 
 uniform sampler2DRect sampler_shadow_map;
 
-uniform sampler2D sampler_brick_diffuse;
-uniform sampler2D sampler_brick_normal;
-
-uniform float spotlight_range;
 uniform vec3 spotlight_position;
+uniform float spotlight_range;
 uniform vec3 spotlight_direction;
 uniform float spotlight_coneangle;
 
 uniform mat4 light_view_projection_xform;
-
-uniform int shadow_pass;
 uniform vec3 camera_position;
 
 out vec3 reflected_light;
@@ -35,23 +30,6 @@ vec3 SpotLight(vec3 worldPosition, vec3 worldNormal, vec3 position, vec3 directi
     return lighting;
 }
 
-vec3 GetMaterialColorFromID(float materialIndex)
-{
-	// curtains
-	if (materialIndex > 0.15f && materialIndex < 0.25f)
-		return vec3(1.0f, 0.0f, 0.0f);
-
-	// plants
-	if (materialIndex > 0.25f && materialIndex < 0.35f)
-		return vec3(0.2f, 0.8f, 0.2f);
-
-	// poles
-	if (materialIndex > 0.35f && materialIndex < 0.45f)
-		return vec3(0.8f, 0.8f, 0.2f);
-
-	return vec3(0.8f, 0.8f, 0.8f);
-}
-
 vec3 SpotLightPass()
 {
 	ivec2 p = ivec2(gl_FragCoord.x, gl_FragCoord.y);
@@ -60,10 +38,7 @@ vec3 SpotLightPass()
 	vec3 worldNormal = texelFetch(sampler_world_normal, p).xyz;
 	vec4 materialInfo = texelFetch(sampler_material_info, p);
 
-	vec3 materialColor = GetMaterialColorFromID(materialInfo.z);
-	float materialShininess = materialInfo.w;
-
-	vec3 lighting = SpotLight(worldPosition, worldNormal, spotlight_position, spotlight_direction, spotlight_coneangle * 0.5f, spotlight_range, materialColor);
+	vec3 lighting = SpotLight(worldPosition, worldNormal, spotlight_position, spotlight_direction, spotlight_coneangle * 0.5f, spotlight_range, vec3(1.0f, 1.0f, 1.0f));
 	return lighting;
 }
 
@@ -74,11 +49,11 @@ vec3 Shadow()
 
 	vec4 hpos_from_light = light_view_projection_xform * worldPosition;
     float light_to_point_depth = hpos_from_light.z / hpos_from_light.w;
-    vec2 shadow_texcoord = vec2( 0.5f + 0.5f * hpos_from_light.x / hpos_from_light.w, 0.5f - 0.5f * hpos_from_light.y / hpos_from_light.w );
+    vec2 shadow_texcoord = vec2(hpos_from_light.x / hpos_from_light.w, hpos_from_light.y / hpos_from_light.w);
 
 	int level_of_filtering = 1;
 	int kernal = 1;
-	int texture_size = 256;
+	int texture_size = 1024;
 	float bias = 0.0001f;
 
 	float count = 0.0f;
@@ -98,23 +73,10 @@ vec3 Shadow()
 	return vec3( shadowing / count, shadowing / count, shadowing / count );
 }
 
-vec3 SpotLightShadowPass()
-{
-	ivec2 p = ivec2(gl_FragCoord.x, gl_FragCoord.y);
-	vec4 worldPosition = texelFetch(sampler_world_position, p);
-
-	float depth = worldPosition.z / worldPosition.w;
-
-	return vec3(depth, 0.0f, 0.0f);
-}
-
 void main(void)
 {
-	if (shadow_pass == 0) {
-		vec3 spotLight = SpotLightPass();
-		vec3 shadowAttenuation = Shadow();
-		reflected_light = spotLight * shadowAttenuation;
-	} else {
-		reflected_light = SpotLightShadowPass();
-	}
+	vec3 spotLight = SpotLightPass();
+	reflected_light = spotLight;
+	//vec3 shadowAttenuation = Shadow();
+	//reflected_light = spotLight * shadowAttenuation;
 }
--- src/Light.cpp
+++ src/Light.cpp
@@ -19,13 +19,11 @@ void Light::CalculateWorldMatrix(glm::vec3 sceneUp)
 
 	xform = glm::scale(xform, glm::vec3(m_light.range, m_light.range, m_light.range));
 
+	// store the light world matrix
 	m_lightxform = xform;
-}
 
-void Light::PerformShadowPass(glm::vec3 sceneUp, const Shader *const shader)
-{
 	// Get the projection matrix
-	const glm::mat4 light_projection_xform = glm::perspective(
+	m_lightprojection = glm::perspective(
 		m_light.field_of_view_degrees,
 		1.0f,
 		0.1f,
@@ -33,60 +31,34 @@ void Light::PerformShadowPass(glm::vec3 sceneUp, const Shader *const shader)
 	);
 
 	// get the view matrix
-	const glm::mat4 light_view_xform = glm::lookAt(
+	m_lightview = glm::lookAt(
 		m_light.position,
 		m_light.position + m_light.direction,
 		sceneUp
 	);
+}
 
-	// Instantiate our uniforms
-	glUniform1i(glGetUniformLocation(shader->GetProgram(), "shadow_pass"), 1);	
-	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "viewMatrix"), 1, GL_FALSE, &light_view_xform[0][0]);
-	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "projectionMatrix"), 1, GL_FALSE, &light_projection_xform[0][0]); 
-
-	// enable blending so we don't nuke our directional light pass
-	glEnable(GL_BLEND);
-	glBlendEquation(GL_FUNC_ADD);
-	glBlendFunc(GL_ONE, GL_ONE); 
-
-	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "worldMatrix"), 1, GL_FALSE, &m_lightxform[0][0]);
-
-	// set the current point lights data
-	glUniform1f(glGetUniformLocation(shader->GetProgram(), "spotlight_range"), m_light.range);				
-	glUniform1f(glGetUniformLocation(shader->GetProgram(), "spotlight_coneangle"), (m_light.field_of_view_degrees *0.5f) * 0.017f);	
-	glUniform3fv(glGetUniformLocation(shader->GetProgram(), "spotlight_position"), 1, glm::value_ptr(m_light.position));	
-	glUniform3fv(glGetUniformLocation(shader->GetProgram(), "spotlight_direction"), 1, glm::value_ptr(m_light.direction));	
+void Light::PerformShadowPass( 
+		glm::vec3 sceneUp, 
+		const Shader *const shader, 
+		glm::mat4 view_xform, 
+		glm::mat4 projection_xform, 
+		glm::vec3 camera_position 
+	)
+{
+	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "viewMatrix"), 1, GL_FALSE, &m_lightview[0][0]);
+	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "projectionMatrix"), 1, GL_FALSE, &m_lightprojection[0][0]); 
+	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "worldMatrix"), 1, GL_FALSE, &m_lightxform[0][0]);	
 }
 
 void Light::PerformLightPass( glm::vec3 sceneUp, const Shader *const shader, glm::mat4 view_xform, glm::mat4 projection_xform, glm::vec3 camera_position )
 {
-	// Get the projection matrix
-	const glm::mat4 light_projection_xform = glm::perspective(
-		m_light.field_of_view_degrees,
-		1.0f,
-		0.1f,
-		m_light.range
-		);
-
-	// get the view matrix
-	const glm::mat4 light_view_xform = glm::lookAt(
-		m_light.position,
-		m_light.position + m_light.direction,
-		sceneUp
-	);
-
-	const glm::mat4 light_view_projection = light_view_xform * light_projection_xform;
+	const glm::mat4 light_view_projection = m_lightview * m_lightprojection;
 
 	// Instantiate our uniforms
-	glUniform1i(glGetUniformLocation(shader->GetProgram(), "shadow_pass"), 0);	
 	glUniform3fv(glGetUniformLocation(shader->GetProgram(), "camera_position"), 1, glm::value_ptr(camera_position));	
 	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "viewMatrix"), 1, GL_FALSE, &view_xform[0][0]);
 	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "projectionMatrix"), 1, GL_FALSE, &projection_xform[0][0]); 
-
-	// enable blending so we don't nuke our directional light pass
-	glEnable(GL_BLEND);
-	glBlendEquation(GL_FUNC_ADD);
-	glBlendFunc(GL_ONE, GL_ONE); 
 
 	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "worldMatrix"), 1, GL_FALSE, &m_lightxform[0][0]);
 	glUniformMatrix4fv(glGetUniformLocation(shader->GetProgram(), "light_view_projection_xform"), 1, GL_FALSE, &light_view_projection[0][0]);
