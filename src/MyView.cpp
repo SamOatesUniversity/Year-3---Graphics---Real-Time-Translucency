5ddd26acf34619f40866b84702bbfce0408b6e4d
samoatesgames <samoatesgames@75805131-b4cb-4ddf-9f19-764025d2d572>
2013-04-16 14:02:39 +0000

    Fix error in shadow buffer creation.

--- src/MyView.cpp
+++ src/MyView.cpp
@@ -292,7 +292,7 @@ void MyView::CreateShadowBuffer(
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, m_shadowbuffer.texture[TranslucencyTexture::xin], 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT3, GL_TEXTURE_2D, m_shadowbuffer.texture[TranslucencyTexture::xin], 0);
 	
 	GLenum drawBufs[TranslucencyTexture::noof];
 	drawBufs[TranslucencyTexture::depth] = GL_COLOR_ATTACHMENT0;
